badeb7bc4f3d7efbc64077ffd9b043db43cc680a
PavelShpagin <pavelandrewshpagin@gmail.com>
2023-11-12 13:48:34 +0200

    Add project files.

---
+++ Decorator/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+namespace Decorator.Examples
+{
+    class MainApp
+    {
+        static void Main()
+        {
+			ConcreteChristmasTree c = new ConcreteChristmasTree();
+			ConcreteChristmasTreeA d1 = new ConcreteChristmasTreeA();
+			ConcreteChristmasTreeB d2 = new ConcreteChristmasTreeB();
+
+            // Link decorators
+            d1.SetComponent(c);
+            d2.SetComponent(d1);
+
+            d2.Decorate();
+
+            // Wait for user
+            Console.Read();
+        }
+    }
+
+    abstract class ChristmasTree
+    {
+        protected bool decorationsSet = false;
+        public abstract void Decorate();
+    }
+
+    class ConcreteChristmasTree : ChristmasTree
+	{
+        public override void Decorate()
+        {
+            decorationsSet = true;
+			Console.WriteLine("ConcreteChristmasTree decorated!");
+        }
+    }
+    // "Decorator"
+    abstract class Decorator : ChristmasTree
+	{
+        protected ChristmasTree component;
+        protected string garland;
+
+		public void SetComponent(ChristmasTree component)
+        {
+            this.component = component;
+        }
+        public override void Decorate()
+        {
+            if (component != null)
+            {
+                component.Decorate();
+            }
+        }
+    }
+
+    class ConcreteChristmasTreeA : Decorator
+    {
+		public override void Decorate()
+        {
+            base.Decorate();
+			garland = "Red garland";
+            Console.WriteLine($"{garland} set with ConcreteChristmasTreeA.Decorate()");
+        }
+    }
+
+    class ConcreteChristmasTreeB : Decorator
+    {
+		public override void Decorate()
+		{
+			base.Decorate();
+			garland = "Blue garland";
+			Console.WriteLine($"{garland} set with ConcreteChristmasTreeA.Decorate()");
+		}
+    }
+}
---
+++ PrototypeFigure/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrototypeFigure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            IFigure figure1 = new Rectangle(10, 20);
+			IFigure figure2 = new Circle(15);
+			IFigure figure3 = new Triangle(3, 4, 5);
+            IFigure clonedFigure = figure1.Clone();
+            figure1.GetInfo();
+            clonedFigure.GetInfo();
+            clonedFigure = figure2.Clone();
+			figure2.GetInfo();
+			clonedFigure.GetInfo();
+            clonedFigure = figure3.Clone();
+			figure3.GetInfo();
+			clonedFigure.GetInfo();
+			Console.Read();
+        }
+    }
+
+    interface IFigure
+    {
+        IFigure Clone();
+        void GetInfo();
+    }
+    class Rectangle : IFigure
+    {
+        int width;
+        int height;
+        public Rectangle(int w, int h)
+        {
+            width = w;
+            height = h;
+        }
+        public IFigure Clone()
+        {
+            return new Rectangle(this.width, this.height);
+        }
+        public void GetInfo()
+        {
+            Console.WriteLine("Прямокутник довжиною {0} и шириною {1}", height, width);
+        }
+    }
+    class Circle : IFigure
+    {
+        int radius;
+        public Circle(int r)
+        {
+            radius = r;
+        }
+        public IFigure Clone()
+        {
+            return new Circle(this.radius);
+        }
+        public void GetInfo()
+        {
+            Console.WriteLine("Круг радіусом {0}", radius);
+        }
+    }
+
+	class Triangle : IFigure
+	{
+		int sideAB;
+		int sideBC;
+        int sideCA;
+		public Triangle(int sideAB, int sideBC, int sideCA)
+		{
+			this.sideAB = sideAB;
+			this.sideBC = sideBC;
+			this.sideCA = sideCA;
+		}
+		public IFigure Clone()
+		{
+			return new Triangle(this.sideAB, this.sideBC, this.sideCA);
+		}
+		public void GetInfo()
+		{
+			Console.WriteLine("Трикутник зі сторонами AB: {0}, BC: {1}, CA: {2}", sideAB, sideBC, sideCA);
+		}
+	}
+}
